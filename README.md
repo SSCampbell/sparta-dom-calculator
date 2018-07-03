40346252c0ebb721df0cb833792950b74a7afbd0
unknown <Scampbell@spartaglobal.com>
2018-07-04 00:35:47 +0100

    README.file: Updated the readme file to document the journey of the project

--- README.md
+++ README.md
@@ -1,11 +1,22 @@
-document.addEventListener("DOMContentLoaded", function(){
+# Sparta Global Academy Homework -- DoM Calculator
 
-  var myButton = document.getElementById("myButton");
+## Project Review:
 
-  myButton.addEventListener("click", handleClick);
+### In this project I had to modify a Calculator on HTML using the DoM JavaScript skills currently reviewed to find the correct elements in the html file and edit their files to direct input/outputs to the screen.
 
-  function handleClick() {
-    console.log(this);
-  }
+## Project Download:  
+### Download from the files created and transferred to GITHUB
+### The repository was created on GITHUB and cloned to access from terminal  
+### Used a JS file & HTML file
 
-  var myForm = document.getElementById("myForm");
+## Using JavaScript tools such as:
+```JavaScript
+var buttonNum = document.getElementsByClassName("buttonNum");
+var operatorBtn = document.getElementsByClassName("operator");
+
+buttonNum[8].addEventListener('click', function(event){
+  event.stopPropagation();
+  textBox.innerHTML = "3";})
+```
+
+## Modifying the Calculator at the beginning stages was good. Using the right procedures to access the correct classes in the HTML was useful to update and assign each button to the Calculator. The textbox was altered to display the buttons pressed into the Calculator. The only problem I had was outputting the sum created by the user. For example "3 + 3 = 6", I need to review how the program uses the operations to sum both numbers together.
--- js/calc.js
+++ js/calc.js
@@ -1,77 +1,212 @@
 document.addEventListener("DOMContentLoaded", function(){
 var textBox = document.getElementById("screen");
-  var buttonNum = document.getElementsByClassName("buttonNum");
-  var operatorBtn = document.getElementsByClassName("operator");
-  console.log(operatorBtn);
+var buttonNum = document.getElementsByClassName("buttonNum");
+var operatorBtn = document.getElementsByClassName("operator");
+var clearBtn = document.getElementsByClassName("buttonClear");
+var equalsBtn = document.getElementsByClassName("equals");
+var numA = 0;
+var numB = 0;
+var opC = "";
+
+
+  clearBtn[0].addEventListener('click', function(event){
+    event.stopPropagation();
+    textBox.innerHTML = " ";
+
+
+  });
+
   buttonNum[8].addEventListener('click', function(event){
     event.stopPropagation();
     textBox.innerHTML = "3";
+
+    if (numA == " ") {
+      numA = 3;
+    }
+
+    else {
+      numB = 3;
+    }
 
 });
 
 buttonNum[7].addEventListener('click', function(event){
   event.stopPropagation();
   textBox.innerHTML = "2";
+  if (numA == " ") {
+    numA = 2;
+  }
+
+  else {
+    numB = 2;
+  }
 });
 
 buttonNum[6].addEventListener('click', function(event){
   event.stopPropagation();
   textBox.innerHTML = "1";
+  if (numA == " ") {
+    numA = 1;
+  }
+
+  else {
+    numB = 1;
+  }
 });
 
 buttonNum[5].addEventListener('click', function(event){
   event.stopPropagation();
   textBox.innerHTML = "6";
+  if (numA == " ") {
+    numA = 6;
+  }
+
+  else {
+    numB = 6;
+  }
 });
 
 buttonNum[4].addEventListener('click', function(event){
   event.stopPropagation();
   textBox.innerHTML = "5";
+  if (numA == " ") {
+    numA = 5;
+  }
+
+  else {
+    numB = 5;
+  }
 });
 
 buttonNum[3].addEventListener('click', function(event){
   event.stopPropagation();
   textBox.innerHTML = "4";
+  if (numA == " ") {
+    numA = 4;
+  }
+
+  else {
+    numB = 4;
+  }
 });
 
 buttonNum[2].addEventListener('click', function(event){
   event.stopPropagation();
   textBox.innerHTML = "9";
+  if (numA == " ") {
+    numA = 9;
+  }
+
+  else {
+    numB = 9;
+  }
 });
 
 buttonNum[1].addEventListener('click', function(event){
   event.stopPropagation();
   textBox.innerHTML = "8";
+  if (numA == " ") {
+    numA = 8;
+  }
+
+  else {
+    numB = 8;
+  }
+});
+
+buttonNum[0].addEventListener('click', function(event){
+  event.stopPropagation();
+  textBox.innerHTML = "7";
+  if (numA == " ") {
+    numA = 7;
+  }
+
+  else {
+    numB = 7;
+  }
 });
 
 operatorBtn[0].addEventListener('click', function(event){
   event.stopPropagation();
   textBox.innerHTML = "*";
+  if (opC == " ") {
+    opC = "*";
+  }
+  else {
+
+  }
 });
 
  operatorBtn[1].addEventListener('click', function(event){
    event.stopPropagation();
    textBox.innerHTML = "/";
+   if (opC == " ") {
+     opC = "/";
+   }
+   else {
+
+   }
  });
 
  operatorBtn[2].addEventListener('click', function(event){
    event.stopPropagation();
    textBox.innerHTML = "-";
+   if (opC == " ") {
+     opC = "-";
+   }
+   else {
+
+   }
  });
 
  operatorBtn[3].addEventListener('click', function(event){
    event.stopPropagation();
    textBox.innerHTML = "+";
+   if (opC == " ") {
+     opC = "+";
+   }
+   else {
+
+   }
  });
 
+if (numA !== 0  && numB !== 0){
+ equalsBtn[0].addEventListener('click', function(event){
+   event.stopPropagation();
+   if (opC == "+")
+   {
+    textBox.innerHTML = numA + numB;
+
+   }
+
+   else if (opC == "-")
+   {
+     textBox.innerHTML = numA - numB;
+
+   }
+
+   else if (opC == "*")
+   {
+     textBox.innerHTML = numA * numB;
+
+   }
+
+   else if (opC == "/")
+   {
+     textBox.innerHTML = numA / numB;
+
+   }
+
+ })}
+ 
 
 
+ });
 
+ var numA = 0;
+ var numB = 0;
+ var opC = "";
 
-
-
-
-})
 
   // var equals = document.getElementById("equals");
   //
